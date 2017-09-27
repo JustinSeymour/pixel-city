555d0e3e1d56bcf3faf16053ce4f5f72aa66de2f
Justin Seymour <justin@3wmholdings.com>
2017-09-27 11:36:19 +0200

    Lesson09 added collectionview programatically

---
+++ pixel-city/View/PhotoCell.swift
@@ -0,0 +1,21 @@
+//
+//  PhotoCell.swift
+//  pixel-city
+//
+//  Created by Justin Seymour on 2017/09/27.
+//  Copyright © 2017 Justin Seymour. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
